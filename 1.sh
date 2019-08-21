f501d70a23980a86f421c0c2f135c1abd759136a
fngoo <38164287+fngoo@users.noreply.github.com>
2019-08-22 01:44:39 +0800

    sh

--- 1.sh
+++ 1.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 apt update
-yes|apt-get install git gcc make libpcap-dev nano zlib*
+yes|apt-get install git gcc g++ make libpcap-dev nano curl zlib* openssl libssl-dev libsqlite3-dev
+
 yes|apt-get install ruby
 yes|apt-get install ruby-dev
-yes|apt install golang-go
-#apt-get install -y build-essential checkinstall libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev zlib1g-dev openssl libffi-dev python3-dev python3-setuptools wget
-
 var=$(ruby -v|grep -oP ".\..\..")
 cd /etc/alternatives
 ln -sf /usr/bin/ruby$var ruby
 
+var=$(curl -L golang.org/dl/|grep -oP "(?<=\<a\ class\=\"download\ downloadBox\"\ href\=\").*?(?=linux\-amd64\.tar\.gz\"\>)")
+last=linux-amd64.tar.gz && var=$var$last && wget $var && load=${var//https\:\/\/dl\.google\.com\/go\//} && tar -xzf $load -C /usr/local
 echo 'export EDITOR=nano'>>~/.profile
 echo 'export GOROOT=/usr/local/go'>>~/.profile
 mkdir ~/GOPATH
@@ -18,16 +18,14 @@ echo 'export GOPATH=/root/GOPATH'>>~/.profile
 echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH/bin'>>~/.profile
 source ~/.profile
 
-wget https://www.python.org/ftp/python/3.7.1/Python-3.7.1rc2.tgz
-tar zxvf Python-3.7.1rc2.tgz
-cd Python-3.7.1rc2
-./configure
-make
-make install
+wget https://www.python.org/ftp/python/3.6.9/Python-3.6.9.tgz
+tar zxvf Python-3.6.9.tgz
+cd Python-3.6.9
+./configure --enable-loadable-sqlite-extensions && make &&  make install
 rm -rf /usr/bin/python3
 rm -rf /usr/bin/pip3
-ln -s /usr/local/bin/python3.7 /usr/bin/python3
-ln -s /usr/local/bin/pip3.7 /usr/bin/pip3
+ln -s /usr/local/bin/python3.6 /usr/bin/python3
+ln -s /usr/local/bin/pip3.6 /usr/bin/pip3
 
 python3 -m pip install --upgrade pip
 wget https://bootstrap.pypa.io/get-pip.py
@@ -36,14 +34,14 @@ rm -r get-pip.py
 
 mkdir ~/script
 
+cd ~/
 python3 -m pip install --upgrade urlwatch
-python3 -m pip install pyyaml minidb requests keyring appdirs lxml cssselect
 urlwatch
 git clone https://github.com/fngoo/txt
 cd txt
 > /root/.config/urlwatch/urlwatch.yaml
 cat 1.txt >/root/.config/urlwatch/urlwatch.yaml
-rm 1.txt
+rm -r ~/txt
 
 mkdir ~/script/0_subdomain
 cd ~/script/0_subdomain
@@ -87,11 +85,7 @@ cd ~/script/6_port
 git clone https://github.com/gm09519/host2ip
 cd host2ip
 sed -e "s/ip\=\"N\/A\"/continue/g" host2ip.py|tee host2ip.py
-git clone https://github.com/nmap/nmap
-cd nmap
-./configure
-make
-make install
+yes|apt install nmap
 cd ~/script/6_port
 git clone https://github.com/robertdavidgraham/masscan
 cd masscan
