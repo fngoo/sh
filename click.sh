0d43eb4072c2e37a88fbb479c25c67121216636e
fngoo <38164287+fngoo@users.noreply.github.com>
2019-08-28 05:29:52 +0800

    Update click.sh

--- click.sh
+++ click.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 screen -LS 1
-cd /root/sh ; sh sh.sh ; cd /root/sh ; sh env.sh ; cd /root/sh ; sh exe.sh
+cd /root/sh ; sh sh.sh ; cd /root/sh ; sh env.sh
+
+cd /root/
+yes|apt remove sendmail
+yes|apt remove postfix
+yes|apt install sharutils mailutils sendmail
+yes|apt install heirloom-mailx
+
+rm -r /root/sh ; cd /root
+while true
+do
+git clone https://github.com/fngoo/sh
+sh sh/sh.sh ; sh sh/exe.sh
+rm -r /root/sh ; cd /root
+done
