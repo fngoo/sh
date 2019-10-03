05a71163b09dd2a7c35837c93268724b68624f09
fngoo <38164287+fngoo@users.noreply.github.com>
2019-10-03 23:46:43 +0800

    Update click_one.sh

--- click_one.sh
+++ click_one.sh
@@ -115,7 +115,7 @@ go get github.com/tomnomnom/waybackurls
 mkdir /root/script/4_getjs
 cd /root/script/4_getjs
 git clone https://github.com/FortyNorthSecurity/EyeWitness
-cd EyeWitness/setup ; bash setup.sh ; bash setup.sh
+cd EyeWitness/setup ; bash setup.sh ; bash setup.sh ; pip install --upgrade pyasn1-modules
 cd /root/
 go get github.com/003random/getJS
 
