3080f22ef57c83b265221adb153249f54a4aa7a7
fngoo <38164287+fngoo@users.noreply.github.com>
2019-11-02 09:49:22 +0800

    Update click_one.sh

--- click_one.sh
+++ click_one.sh
@@ -47,7 +47,7 @@ git clone https://github.com/fngoo/xunfeng ; sh xunfeng/xunfeng.sh ; rm -r /root
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i ./google-chrome-stable_current_amd64.deb
-apt-get -f install
+yes|apt-get -f install
 rm google-chrome-stable_current_amd64.deb
 
 
