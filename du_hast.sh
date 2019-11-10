cf3a47aa2ad80473c1331b1fc488090772e7af8e
fngoo <38164287+fngoo@users.noreply.github.com>
2019-11-10 14:44:26 +0800

    Update du_hast.sh

--- du_hast.sh
+++ du_hast.sh
@@ -1,19 +1,14 @@
+yes|apt install screen
 screen -L -dmS $"bash"
 cmd=$"bash /root/sh/click_one.sh"
 screen -x -S bash -X stuff "$cmd"
 screen -x -S bash -X stuff $'\n'
-
-echo "du"
 
 screen -dmS $"http"
 cmd=$"python -m SimpleHTTPServer 62345"
 screen -x -S http -X stuff "$cmd"
 screen -x -S http -X stuff $'\n'
 
-echo "du hast"
-echo "Rammstein - Du Hast"
-echo ""
-echo ""
-echo ""
+
 echo "http://ip:62345"
 echo "tail -f screenlog.0"
