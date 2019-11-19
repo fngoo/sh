d83323ee8fb83d65169a1d9f428100f5026e812d
fngoo <38164287+fngoo@users.noreply.github.com>
2019-11-19 21:50:47 +0800

    Update click_one.sh

--- click_one.sh
+++ click_one.sh
@@ -29,6 +29,7 @@ wget https://www.python.org/ftp/python/3.6.9/Python-3.6.9.tgz
 tar zxvf Python-3.6.9.tgz && rm -rf Python-3.6.9.tgz
 cd Python-3.6.9
 ./configure --enable-loadable-sqlite-extensions ; make ;  make install
+apt-get install python3-pip
 rm -rf /usr/bin/python3
 rm -rf /usr/bin/pip3
 ln -s /usr/local/bin/python3.6 /usr/bin/python3
