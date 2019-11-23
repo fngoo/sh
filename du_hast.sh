1333af9f00d6e9456c2102b4da400b56be17604e
fngoo <38164287+fngoo@users.noreply.github.com>
2019-11-23 23:15:43 +0800

    Update du_hast.sh

--- du_hast.sh
+++ du_hast.sh
@@ -9,5 +9,7 @@ cmd=$"python -m SimpleHTTPServer 62345"
 screen -x -S http -X stuff "$cmd"
 screen -x -S http -X stuff $'\n'
 
-
+clear
 echo "output.zip------cd /root/zip" ; echo "output.txt------cd /root/z_juice" ; echo "output.web------http://ip:62345/z_juice"
+echo "bash /root/script/webhook.sh"
+#a=`date "+%Y-%m-%d_%H:%M:%S"` ; echo "curl -X POST -H \"Content-type:application/json\" --data '{\"text\":\"${a}\"}' https://hooks.slack.com/services/token/token/token" > /root/script/slack.sh ; bash /root/script/slack.sh ; rm /root/script/slack.sh
