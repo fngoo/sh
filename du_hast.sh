041e1657da523782ead33fca8b73509f71a4e416
fngoo <38164287+fngoo@users.noreply.github.com>
2019-11-25 20:43:21 +0800

    Update du_hast.sh

--- du_hast.sh
+++ du_hast.sh
@@ -14,7 +14,7 @@ clear
 echo "output.zip------cd /root/zip" ; echo "output.txt------cd /root/z_juice" ; echo "output.web------http://ip:62345/z_juice"
 echo ""
 echo ""
-head -17 /root/sh/du_hast.sh | tail -1
+head -18 /root/sh/du_hast.sh | tail -1
 #   time=`date "+%Y-%m-%d_%H:%M:%S"` ; echo "curl -X POST -H \"Content-type:application/json\" --data '{\"text\":\"${time}\"}' https://hooks.slack.com/services/token/token/token" > /root/script/slack.sh ; bash /root/script/slack.sh ; rm /root/script/slack.sh
 echo ""
 echo "replace URL to your slack webhook , then copy into /root/script/webhook.sh"
