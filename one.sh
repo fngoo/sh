efa8ffcae50b1ee718b58d410f2cf8ba40dd6d7c
fngoo <38164287+fngoo@users.noreply.github.com>
2019-11-28 01:40:09 +0800

    Update one.sh

--- one.sh
+++ one.sh
@@ -26,9 +26,9 @@ for one in `cat $var`
 do
 echo "$one" > /root/script/one.txt
 txt=/root/script/one.txt
-cd /root/script/0_subdomain/Sublist3r ; python3 sublist3r.py -v -d $one -o /root/script/0_subdomain/0_sublist.txt ; sleep 6
-cd /root/script/0_subdomain/altdns ; altdns -r -n -i $txt -w words.txt -o 2.txt -s 3.txt ; cat 3.txt | grep -v "\[" | grep -oP ".*(?=\:)" >> /root/script/subfinder.txt ; > 2.txt ; > 3.txt
-cat /root/script/0_subdomain/0_sublist.txt >> /root/script/subfinder.txt ; > /root/script/0_subdomain/0_sublist.txt ; sort -u /root/script/subfinder.txt -o /root/script/subfinder.txt
+cd /root/script/0_subdomain/Sublist3r ; python3 sublist3r.py -v -d $one -o /root/script/0_subdomain/0_sublist.txt ; sleep 6 ; cat /root/script/0_subdomain/0_sublist.txt >> /root/script/subfinder.txt ; > /root/script/0_subdomain/0_sublist.txt
+cd /root/script/0_subdomain/altdns ; altdns -r -n -i $txt -w words.txt -o 2.txt -s 3.txt ; cat 3.txt | grep -v "\[" | grep -oP ".*(?=\:)" >> /root/script/subfinder.txt ; > 2.txt ; > 3.txt ; sort -u /root/script/subfinder.txt -o /root/script/subfinder.txt ; altdns -r -n -i /root/script/subfinder.txt -w words.txt -o 2.txt -s 3.txt ; cat 3.txt | grep -v "\[" | grep -oP ".*(?=\:)" >> /root/script/subfinder.txt
+sort -u /root/script/subfinder.txt -o /root/script/subfinder.txt
 rm /root/script/one.txt
 wc -l /root/script/subfinder.txt
 done
@@ -99,9 +99,9 @@ for one in `cat $var`
 do
 echo "$one" > /root/script/one.txt
 txt=/root/script/one.txt
-cd /root/script/0_subdomain/Sublist3r ; python3 sublist3r.py -v -d $one -o /root/script/0_subdomain/0_sublist.txt ; sleep 6
-cd /root/script/0_subdomain/altdns ; altdns -r -n -i $txt -w words.txt -o 2.txt -s 3.txt ; cat 3.txt | grep -v "\[" | grep -oP ".*(?=\:)" >> /root/script/subfinder.txt ; > 2.txt ; > 3.txt
-cat /root/script/0_subdomain/0_sublist.txt >> /root/script/subfinder.txt ; > /root/script/0_subdomain/0_sublist.txt ; sort -u /root/script/subfinder.txt -o /root/script/subfinder.txt
+cd /root/script/0_subdomain/Sublist3r ; python3 sublist3r.py -v -d $one -o /root/script/0_subdomain/0_sublist.txt ; sleep 6 ; cat /root/script/0_subdomain/0_sublist.txt >> /root/script/subfinder.txt ; > /root/script/0_subdomain/0_sublist.txt
+cd /root/script/0_subdomain/altdns ; altdns -r -n -i $txt -w words.txt -o 2.txt -s 3.txt ; cat 3.txt | grep -v "\[" | grep -oP ".*(?=\:)" >> /root/script/subfinder.txt ; > 2.txt ; > 3.txt ; sort -u /root/script/subfinder.txt -o /root/script/subfinder.txt ; altdns -r -n -i /root/script/subfinder.txt -w words.txt -o 2.txt -s 3.txt ; cat 3.txt | grep -v "\[" | grep -oP ".*(?=\:)" >> /root/script/subfinder.txt
+sort -u /root/script/subfinder.txt -o /root/script/subfinder.txt
 rm /root/script/one.txt
 wc -l /root/script/subfinder.txt
 done
