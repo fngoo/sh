fca33800f8f59fdb57909baa436d2765511741a4
fngoo <38164287+fngoo@users.noreply.github.com>
2019-11-29 00:46:13 +0800

    Update du_hast.sh

--- du_hast.sh
+++ du_hast.sh
@@ -26,7 +26,12 @@ head -24 /root/sh/du_hast.sh | tail -1 >> /root/sh/readme.txt
 echo "" >> /root/sh/readme.txt
 echo "replace URL to your blind payload , vi /root/script/xss.sh" >> /root/sh/readme.txt
 echo "OR NOT, then run without potential blind vuln" >> /root/sh/readme.txt
-echo -e "\n"
+echo "" >> /root/sh/readme.txt
+echo "" >> /root/sh/readme.txt
+echo "" >> /root/sh/readme.txt
+echo "" >> /root/sh/readme.txt
+echo "" >> /root/sh/readme.txt
+echo "" >> /root/sh/readme.txt
 echo "" >> /root/sh/readme.txt
 echo "" >> /root/sh/readme.txt
 echo "" >> /root/sh/readme.txt
