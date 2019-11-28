411707d84963e4a2da55ebc5888c95dc6cee1451
fngoo <38164287+fngoo@users.noreply.github.com>
2019-11-29 01:42:42 +0800

    Update one.sh

--- one.sh
+++ one.sh
@@ -29,24 +29,32 @@ txt=/root/script/one.txt
 cd /root/script/0_subdomain/Sublist3r ; python3 sublist3r.py -v -d $one -o /root/script/0_subdomain/0_sublist.txt ; sleep 6 ; cat /root/script/0_subdomain/0_sublist.txt >> /root/script/subfinder.txt ; > /root/script/0_subdomain/0_sublist.txt
 if [ -s /root/script/subfinder.txt ]
 then
-cat /root/script/subfinder.txt | massdns -r /root/script/0_subdomain/massdns/lists/resolvers.txt --root -t A -s 1666 -o S -w results.txt ; awk -F ". " '{print $1}' results.txt > wordlist-filtered.txt && mv wordlist-filtered.txt results.txt ; sort -u results.txt -o results.txt ; cat results.txt >> /root/script/massdns.txt ; rm results.txt ; sort -u /root/script/massdns.txt -o /root/script/massdns.txt
-line=`cat /root/script/massdns.txt | wc -l`
-if [ $line -gt 66666 ]
+cat /root/script/subfinder.txt | massdns -r /root/script/0_subdomain/massdns/lists/resolvers.txt --root -t A -s 1666 -o S -w results.txt ; awk -F ". " '{print $1}' results.txt > wordlist-filtered.txt && mv wordlist-filtered.txt results.txt ; sort -u results.txt -o results.txt ; cat results.txt >> /root/script/massdns.txt ; rm results.txt ; sort -u /root/script/massdns.txt -o /root/script/massdns.txt ; ag -oP "\ A.*" /root/script/massdns.txt > ag.txt ; sort -u ag.txt -o ag.txt
+for line in `cat ag.txt`
+do
+num=`ag -oP "$line" /root/script/massdns.txt | wc -l`
+if [ $num -gt 666 ]
 then
-> /root/script/massdns.txt
+grep -v "$line" /root/script/massdns.txt > grep_v.txt ; mv grep_v.txt /root/script/massdns.txt
 fi
+done
+> ag.txt
 fi
 cat /root/script/massdns.txt >> $var ; rm /root/script/massdns.txt ; cat /root/script/subfinder.txt >> $var ; > /root/script/subfinder.txt
 cd /root/script/0_subdomain/altdns ; altdns -i $txt -w words_large.txt -o 2.txt ; cat 2.txt >> /root/script/subfinder.txt ; > 2.txt ; sort -u /root/script/subfinder.txt -o /root/script/subfinder.txt
 if [ -s /root/script/subfinder.txt ]
 then
-cat /root/script/subfinder.txt | massdns -r /root/script/0_subdomain/massdns/lists/resolvers.txt --root -t A -s 1666 -o S -w results.txt ; awk -F ". " '{print $1}' results.txt > wordlist-filtered.txt && mv wordlist-filtered.txt results.txt ; sort -u results.txt -o results.txt ; cat results.txt >> /root/script/massdns.txt ; rm results.txt ; sort -u /root/script/massdns.txt -o /root/script/massdns.txt
+cat /root/script/subfinder.txt | massdns -r /root/script/0_subdomain/massdns/lists/resolvers.txt --root -t A -s 1666 -o S -w results.txt ; awk -F ". " '{print $1}' results.txt > wordlist-filtered.txt && mv wordlist-filtered.txt results.txt ; sort -u results.txt -o results.txt ; cat results.txt >> /root/script/massdns.txt ; rm results.txt ; sort -u /root/script/massdns.txt -o /root/script/massdns.txt ; ag -oP "\ A.*" /root/script/massdns.txt > ag.txt ; sort -u ag.txt -o ag.txt
 > /root/script/one.txt ; > /root/script/subfinder.txt
-line=`cat /root/script/massdns.txt | wc -l`
-if [ $line -gt 66666 ]
+for line in `cat ag.txt`
+do
+num=`ag -oP "$line" /root/script/massdns.txt | wc -l`
+if [ $num -gt 666 ]
 then
-> /root/script/massdns.txt
+grep -v "$line" /root/script/massdns.txt > grep_v.txt ; mv grep_v.txt /root/script/massdns.txt
 fi
+done
+> ag.txt
 fi
 cat /root/script/massdns.txt >> $var ; rm /root/script/massdns.txt
 sort -u $var -o $var
@@ -99,24 +107,32 @@ txt=/root/script/one.txt
 cd /root/script/0_subdomain/Sublist3r ; python3 sublist3r.py -v -d $one -o /root/script/0_subdomain/0_sublist.txt ; sleep 6 ; cat /root/script/0_subdomain/0_sublist.txt >> /root/script/subfinder.txt ; > /root/script/0_subdomain/0_sublist.txt
 if [ -s /root/script/subfinder.txt ]
 then
-cat /root/script/subfinder.txt | massdns -r /root/script/0_subdomain/massdns/lists/resolvers.txt --root -t A -s 1666 -o S -w results.txt ; awk -F ". " '{print $1}' results.txt > wordlist-filtered.txt && mv wordlist-filtered.txt results.txt ; sort -u results.txt -o results.txt ; cat results.txt >> /root/script/massdns.txt ; rm results.txt ; sort -u /root/script/massdns.txt -o /root/script/massdns.txt
-line=`cat /root/script/massdns.txt | wc -l`
-if [ $line -gt 66666 ]
+cat /root/script/subfinder.txt | massdns -r /root/script/0_subdomain/massdns/lists/resolvers.txt --root -t A -s 1666 -o S -w results.txt ; awk -F ". " '{print $1}' results.txt > wordlist-filtered.txt && mv wordlist-filtered.txt results.txt ; sort -u results.txt -o results.txt ; cat results.txt >> /root/script/massdns.txt ; rm results.txt ; sort -u /root/script/massdns.txt -o /root/script/massdns.txt ; ag -oP "\ A.*" /root/script/massdns.txt > ag.txt ; sort -u ag.txt -o ag.txt
+for line in `cat ag.txt`
+do
+num=`ag -oP "$line" /root/script/massdns.txt | wc -l`
+if [ $num -gt 666 ]
 then
-> /root/script/massdns.txt
+grep -v "$line" /root/script/massdns.txt > grep_v.txt ; mv grep_v.txt /root/script/massdns.txt
 fi
+done
+> ag.txt
 fi
 cat /root/script/massdns.txt >> $var ; rm /root/script/massdns.txt ; cat /root/script/subfinder.txt >> $var ; > /root/script/subfinder.txt
 cd /root/script/0_subdomain/altdns ; altdns -i $txt -w words_large.txt -o 2.txt ; cat 2.txt >> /root/script/subfinder.txt ; > 2.txt ; sort -u /root/script/subfinder.txt -o /root/script/subfinder.txt
 if [ -s /root/script/subfinder.txt ]
 then
-cat /root/script/subfinder.txt | massdns -r /root/script/0_subdomain/massdns/lists/resolvers.txt --root -t A -s 1666 -o S -w results.txt ; awk -F ". " '{print $1}' results.txt > wordlist-filtered.txt && mv wordlist-filtered.txt results.txt ; sort -u results.txt -o results.txt ; cat results.txt >> /root/script/massdns.txt ; rm results.txt ; sort -u /root/script/massdns.txt -o /root/script/massdns.txt
+cat /root/script/subfinder.txt | massdns -r /root/script/0_subdomain/massdns/lists/resolvers.txt --root -t A -s 1666 -o S -w results.txt ; awk -F ". " '{print $1}' results.txt > wordlist-filtered.txt && mv wordlist-filtered.txt results.txt ; sort -u results.txt -o results.txt ; cat results.txt >> /root/script/massdns.txt ; rm results.txt ; sort -u /root/script/massdns.txt -o /root/script/massdns.txt ; ag -oP "\ A.*" /root/script/massdns.txt > ag.txt ; sort -u ag.txt -o ag.txt
 > /root/script/one.txt ; > /root/script/subfinder.txt
-line=`cat /root/script/massdns.txt | wc -l`
-if [ $line -gt 66666 ]
+for line in `cat ag.txt`
+do
+num=`ag -oP "$line" /root/script/massdns.txt | wc -l`
+if [ $num -gt 666 ]
 then
-> /root/script/massdns.txt
+grep -v "$line" /root/script/massdns.txt > grep_v.txt ; mv grep_v.txt /root/script/massdns.txt
 fi
+done
+> ag.txt
 fi
 cat /root/script/massdns.txt >> $var ; rm /root/script/massdns.txt
 sort -u $var -o $var
